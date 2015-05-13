c97cc9fb8a1097f29588e19e56ee2a47652517c5
keis94 <keis.vivi@gmail.com>
2015-05-13 14:51:37 +0900

    send()の更新

--- User.java
+++ User.java
@@ -1,7 +1,14 @@
+import java.util.scanner;
+
 public class User{
 	int money;
 
     boolean send(){
+        Scanner scanner = new Scanner(System.in);
+        String input;
 
+        System.out.println("Aなら1,　Bなら0を入力　>");
+        input = scanner.next();
+        return Integer.parseInt(input) ? true : false;
 	}
 }
