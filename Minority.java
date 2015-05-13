f145cab9be3105bc47e269e6c6213f8364b25d40
graph226 <graph226@gmail.com>
2015-05-13 14:56:39 +0900

    使いそうなメソッドを追加

--- Minority.java
+++ Minority.java
@@ -2,14 +2,26 @@ public class Minority{
 	public static void main(String[] args){
 		int pro;
 		int con;
+		int member;		//how many members
 
 	}
-	public static void counter(boolean state){
+	public static void counter(boolean state){	//user accesses this field
 		if(state){
 			pro++;
 		}else{
 			con++;
 		}
+	}
+
+	public static void judge(){
+		if(pro+con==member){
+			if(pro>con){
+
+			}else{
+				
+			}
+		}
+
 	}
 
     public static boolean receiver(){
