d210fadca42bc6c08694dfcc90de20641dd13159
graph226 <graph226@gmail.com>
2015-06-09 16:34:38 +0900

    テキストフィールドを追加

--- UserGui.java
+++ UserGui.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextPane;
-import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import java.awt.Container;
 import java.awt.BorderLayout;
 
@@ -20,10 +20,18 @@ class UserGui extends JFrame{
 
     JPanel p = new JPanel();
 
+		//ウェルカムメッセージ
     JLabel label1 = new JLabel();
 		label1.setText("<html>Welcome!<br>Tell me your name!");
 
+		//Name入力エリア
+		JLabel label2 = new JLabel("Name");
+		JTextField text1 = new JTextField(10);
+
+
     p.add(label1);
+		p.add(label2);
+		p.add(text1);
 
     Container contentPane = getContentPane();
     contentPane.add(p, BorderLayout.CENTER);
