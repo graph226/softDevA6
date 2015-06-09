4dc0fb094bcbded2074a3b9d67c2a5de0a327383
graph226 <graph226@gmail.com>
2015-06-09 16:56:37 +0900

    テキスト入力画面を追加

--- UserGui.java
+++ UserGui.java
@@ -1,4 +1,4 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextPane;
@@ -8,32 +8,34 @@ import java.awt.BorderLayout;
 
 class UserGui extends JFrame{
   public static void main(String args[]){
-    UserGui frame = new UserGui("少数欠ゲーム");
+    UserGui frame = new UserGui("少数決ゲーム");
     frame.setVisible(true);
   }
 
   UserGui(String title){	//コンストラクタ
     setTitle(title);
-    setBounds(100, 100, 300, 250);
+    setBounds(100, 100, 300, 400);
 		//閉じたときの動作
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JPanel p = new JPanel();
+    JPanel p1 = new JPanel();
 
 		//ウェルカムメッセージ
     JLabel label1 = new JLabel();
-		label1.setText("<html>Welcome!<br>Tell me your name!");
+		label1.setText("<html>Welcome!<br>Tell me your name!<br>");
 
+		JPanel p2 = new JPanel();
 		//Name入力エリア
-		JLabel label2 = new JLabel("Name");
+		JLabel label2 = new JLabel("Name",SwingConstants.LEFT);
 		JTextField text1 = new JTextField(10);
 
 
-    p.add(label1);
-		p.add(label2);
-		p.add(text1);
+    p1.add(label1);
+		p2.add(label2);
+		p2.add(text1);
 
     Container contentPane = getContentPane();
-    contentPane.add(p, BorderLayout.CENTER);
+    contentPane.add(p1, BorderLayout.NORTH);
+    contentPane.add(p2, BorderLayout.CENTER);
   }
 }
